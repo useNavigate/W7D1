171db9526d5f4f5c1fe777e8d69e6e7885a483c7
Steven Notovitz <sjn39@cornell.edu>
2023-02-20 15:15:19 -0500

    Add reset_session_token!

--- practice-for-ch-rails-ninety-nine-cats1-long-practice/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice/app/models/user.rb
@@ -10,14 +10,39 @@
 # u updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  before_validation :ensure_session_token
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
-
-  before_validation :ensure_session_token
 
   def ensure_session_token
     session_token ||= generate_unique_session_token
   end
+
+  def password=(password)
+    password_digest = Bcrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    #convert password_digest from string to bcrypt instance
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    end
+    nil
+  end
+
+  def reset_session_token!
+    session_token = generate_unique_session_token
+    self.save!
+    session_token
+  end
+
+  private
 
   def generate_unique_session_token
     token = SecureRandom::urlsafe_base64
@@ -27,13 +52,8 @@ class User < ApplicationRecord
     token
   end
 
-  def password=(password)
-    password_digest = Bcrypt::Password.create(password)
-    @password = password
+  def ensure_session_token
+    session_token ||= generate_unique_session_token
   end
 
-  def is_password?(password)
-    #converting password_digest(string) => bcrypt instance
-    BCrypt::Password.new(password_digest).is_password?(password)
-  end
 end
