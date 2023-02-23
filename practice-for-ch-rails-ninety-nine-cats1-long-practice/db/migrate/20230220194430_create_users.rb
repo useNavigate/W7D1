6a58f74d794624205993a10ca98745c330f3b00e
Steven Notovitz <sjn39@cornell.edu>
2023-02-23 14:56:24 -0500

    Update 20230220194430_create_users.rb

--- practice-for-ch-rails-ninety-nine-cats1-long-practice/db/migrate/20230220194430_create_users.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice/db/migrate/20230220194430_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, unique: true, null: false, index: true
+      t.string :username, null: false, index: { unique: true }
       t.string :password_digest, null: false
-      t.string :session_token, null: false, unique: true, index: true
+      t.string :session_token, null: false, index: { unique: true }
 
       t.timestamps
     end
